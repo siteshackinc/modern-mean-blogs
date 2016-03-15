da6dd6001d8fd94337a436368cc29f13ff9b8d33
James Ireland <siteshackinc@users.noreply.github.com>
2016-03-15 14:57:57 -0500

    feat(server) added imports

--- server/blogs.server.module.js
+++ server/blogs.server.module.js
@@ -1,2 +1,15 @@
 'use strict';
 
+import blogModel from './models/blogs.server.model.blog';
+import userRoutes from './routes/blogs.server.routes';
+import chalk from 'chalk';
+import config from 'modernMEAN/config';
+
+// Needs to be finished
+//
+//
+
+let service = { init: init };
+
+export default service;
+export { init }
