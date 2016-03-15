cbc17c7389b9c163d1d0cdc92fb89881c0d9745b
James Ireland <siteshackinc@users.noreply.github.com>
2016-03-15 14:55:56 -0500

    feat(server) added imports

--- server/admin.blogs.server.module.js
+++ server/admin.blogs.server.module.js
@@ -1,2 +1,12 @@
 'use strict';
 
+import adminRoutes from './routes/admin.server.routes';
+import { policy } from './policies/admin.server.policy';
+import chalk from 'chalk';
+
+// Needs to be finished
+//
+//
+
+export default service;
+export { init }
