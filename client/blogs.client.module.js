18d5a917cf5bfaa3169069e5be0952ae66add26c
James Ireland <siteshackinc@users.noreply.github.com>
2016-03-16 01:21:44 -0500

    Update blogs.client.module.js

--- client/blogs.client.module.js
+++ client/blogs.client.module.js
@@ -1,7 +1,7 @@
 (function(window, angular) {
   'use strict';
 
-  window.modernMeanApplication.registerModule('blogs', ['core', 'users']);
+  window.modernMeanApplication.registerModule('blogs');
   window.modernMeanApplication.registerModule('blogs.factory');
   window.modernMeanApplication.registerModule('blogs.admin', ['core.admin', 'users.admin']);
   window.modernMeanApplication.registerModule('blogs.routes', ['core.routes', 'blogs.factory']);
